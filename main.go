1aebb2be9bac2b7135a24e6b149b90546a25daa9
A7medAbdien <ahmed.g.abdien@gmail.com>
2024-02-25 02:57:45 +0300

    init - outlining the main file

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// context
+
+	// load local variables
+	// initialize clients
+
+	// listen to events
+
+	// get user massage
+	// get relevant information from the massage
+	// response
+}
